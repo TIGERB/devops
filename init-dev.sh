e266db9f8867032b423b27749b1f146c1fffa53d
TIGERB <tigerbcode@gmail.com>
2017-11-10 16:19:32 +0800

    feat(nginx): add nginx conf demo

--- init-dev.sh
+++ init-dev.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # vim /etc/apt/sources.list
 # :%s/us.archive/cn.archive/g
+# developer env init script for ubuntu
+# author <tigerbcode@gmail.com>
+# blog <TIGERB.cn>
+# version <0.1.0>
+
+# -------------------- params check ------------------------
 
 # define user name
 shell_username="$1"
@@ -12,7 +18,7 @@ then
   exit 1
 fi
 
-# ---------- start --------------
+# -------------------- zsh ------------------------
 
 echo -e "\033[36m Install zsh oh-my-zsh [→] \033[0m"
 
@@ -20,11 +26,19 @@ sudo apt-get install zsh
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# backend develop enviorment
+# -------------------- git ------------------------
+git config --global alias.plom "pull origin master"
+git config --global alias.plod "pull origin develop"
+
+# -------------------- nginx ------------------------
 
 echo -e "\033[36m Install nginx [→] \033[0m"
 
 sudo apt-get install nginx
+
+cp -r ./sites-available /etc/nginx/sites-available
+
+# -------------------- php ------------------------
 
 echo -e "\033[36m Install php [→] \033[0m"
 
@@ -33,6 +47,8 @@ sudo apt-get install php
 echo -e "\033[36m Install php-lib, php-redis, php-gd, php-mongo, composer [→] \033[0m"
 
 sudo apt-get install php7.0-common php-redis php-mongodb php-gd composer
+
+# -------------------- mysql/redis/mongodb ------------------------
 
 echo -e "\033[36m Install mysql [→] \033[0m"
 
@@ -46,7 +62,7 @@ echo -e "\033[36m Install mongodb [→] \033[0m"
 
 sudo apt-get install mongodb
 
-# frontend develop enviorment
+# -------------------- node/yarn ------------------------
 
 echo -e "\033[36m Install node,yarn [→] \033[0m"
 
@@ -55,13 +71,15 @@ sudo apt-get install -y nodejs
 
 sudo npm install -g yarn
 
-# tool
+# -------------------- htop/mycli/ab ------------------------
 
 echo -e "\033[36m Install htop, mycli [→] \033[0m"
 
-sudo apt-get install htop mycli apache2-utils
+sudo apt-get install htop mycli apache2-utils tmux
 
-# Treri dotfiles
+# -------------------- Treri-dotfiles ------------------------
+
+# Treri dotfiles·
 git clone git@github.com:Treri/dotfiles.git && cd dotfiles
 
 ./install.sh tmux
@@ -76,6 +94,8 @@ git clone git@github.com:Treri/dotfiles.git && cd dotfiles
 ./install.sh zsh_plugins_fzf
 ./install.sh zsh_plugins_thefuck
 
+# -------------------- golang ------------------------
+
 # install golang
 echo -e "\033[36m Install golang version:1.9.2 [→] \033[0m"
 
@@ -87,8 +107,28 @@ wget "https://studygolang.com/dl/golang/go$GOVERSION.$OS-$ARCH.tar.gz"
 
 tar -C /usr/local -xzf go$GOVERSION.$OS-$ARCH.tar.gz
 
-echo -e "export PATH=$PATH:/usr/local/go/bin" >> "$HOME/.profile"
+echo -e "export PATH=$PATH:/usr/local/go/bin" >> "$HOME/.profile" && source ~/.profile
 
+sudo add-apt-repository ppa:masterminds/glide && sudo apt-get update
+sudo apt-get install glide
+
+go get github.com/beego/bee
+
+# install supervisor
+echo -e "\033[36m Install supervisor [→] \033[0m"
+sudo apt-get install supervisor && cd /etc/supervisor/conf.d
+
+cat <<EOF
+[program:projectname]
+command = 
+autostart = true
+autorestart = true
+user = $shell_username
+stdout_logfile = 
+EOF
+>> ./demo.conf
+
+# -------------------- user ------------------------
 
 # create user
 echo -e "\033[36m Add user: $shell_username [→] \033[0m"
